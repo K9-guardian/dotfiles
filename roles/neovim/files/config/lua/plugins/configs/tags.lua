31eb19744b7c29c06b18497b34ec01b6e9c6ae65
Krishna Girkar <girkar.km@gmail.com>
2026-05-01 21:19:34 -0700

    i will become a tag guy

--- roles/neovim/files/config/lua/plugins/configs/tags.lua
+++ roles/neovim/files/config/lua/plugins/configs/tags.lua
@@ -1,1 +1,8 @@
+vim.g.gutentags_project_root = { '.gutentags' }
+vim.g.gutentags_add_default_project_roots = 0
+
 vim.g.gutentags_ctags_tagfile = ".tags"
+
+vim.keymap.set('n', 'g]', function()
+   require('fzf-lua').tags({ query = vim.fn.expand('<cword>') })
+end, { desc = 'Fzf-lua tags (always pick)' })
